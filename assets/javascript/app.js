155e71da2b4a127a65cac0e4175d06bd79f587f4
Romeo Amaro <romeoamaro@Romeos-MBP.attlocal.net>
2018-05-18 19:25:10 -0700

    html/css part

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+//Spesudo code
+//1.create a count down timer(game over once the timer hits zero)
+//2.create list of questions
+//3.create 4 options for answers
+//4.create buttons for different answers
+///5.create if/true or false statements for all the questions
+//6.create a total score window that appers when game is document
+//7.find out how to reload game after being document
